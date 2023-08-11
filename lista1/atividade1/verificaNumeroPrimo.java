f44e5f60762852d14e38f06acb2faa2cd21fb3e7
KingMel0 <marcofc6@gmail.com>
2023-08-11 10:39:12 -0300

    Atividade1

---
+++ lista1/atividade1/verificaNumeroPrimo.java
@@ -0,0 +1,11 @@
+package lista1.atividade1;
+
+/**
+ * verificaNumeroPrimo
+ */
+public class verificaNumeroPrimo {
+
+    public static void main(String args[]) {
+        System.out.println("ola mundo");
+    }    
+}
