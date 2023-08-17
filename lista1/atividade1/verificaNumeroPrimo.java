d6608dd9facfac688afbe3f4e5810843dd062519
Marco Tulio <marcofc6@gmail.com>
2023-08-17 13:26:34 -0300

    atividade 1

--- lista1/atividade1/verificaNumeroPrimo.java
+++ lista1/atividade1/verificaNumeroPrimo.java
@@ -1,11 +1,27 @@
-package lista1.atividade1;
+public class VerificadorNumeroPrimo {
 
-/**
- * verificaNumeroPrimo
- */
-public class verificaNumeroPrimo {
+    public static void main(String[] args) {
+        int numero = 17; 
+        int resultado = verificaNumeroPrimo(numero);
+        
+        if (resultado == 1) {
+            System.out.println(numero + " eh numero primo.");
+        } else {
+            System.out.println(numero + " nao eh numero primo.");
+        }
+    }
 
-    public static void main(String args[]) {
-        System.out.println("ola mundo");
-    }    
+    public static int verificaNumeroPrimo(Integer n) {
+
+        if (n <= 1) {
+            return 0;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return 0; 
+            }
+        }
+        return 1; 
+    }
 }
