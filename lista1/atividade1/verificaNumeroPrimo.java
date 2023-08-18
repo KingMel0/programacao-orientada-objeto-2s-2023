966b11f49d0353c21c0b2569645de8c7f1ce7f9e
MARCO TULIO FERREIRA <marco.ferreira@a.ucb.br>
2023-08-18 09:43:16 -0300

    atividade

--- lista1/atividade1/verificaNumeroPrimo.java
+++ lista1/atividade1/verificaNumeroPrimo.java
@@ -1,27 +1,18 @@
-public class VerificadorNumeroPrimo {
+public class Primo {
 
     public static void main(String[] args) {
-        int numero = 31; 
-        int resultado = verificaNumeroPrimo(numero);
-        
-        if (resultado == 1) {
-            System.out.println(numero + " eh numero primo.");
-        } else {
-            System.out.println(numero + " nao eh numero primo.");
-        }
-    }
-
-    public static int verificaNumeroPrimo(Integer n) {
-
-        if (n <= 1) {
-            return 0;
-        }
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return 0; 
+        int n = 31; 
+        int eh_primo = 1;
+        if (int i = 3; i < n; i++) {
+            if(n % i == 0){
+                eh_primo=0;
             }
+        } 
         }
-        return 1; 
+
+        System.out.printIn(eh_primo);
     }
-}
+
+    
+    
+
